ed819f1b165a9fff1522e3f71b9ef108980429c4
quanggdungg0609 <truongquangdung0609@gmail.com>
2022-04-10 14:00:14 +0200

    first commit

---
+++ todolist.py
@@ -0,0 +1,7 @@
+from os import name
+from todolist import create_app
+
+app=create_app()
+
+if __name__=="__main__":
+    app.run(debug=True)
---
+++ todolist/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import os
+from dotenv import load_dotenv
+from .user import user
+from .views import views
+
+load_dotenv()
+SECRET_KEY=os.environ.get("SECRET_KEY")
+DB_NAME= os.environ.get("DB_NAME")
+
+
+
+def create_app():
+    app=Flask(__name__)
+    app.config['SECRET_KEY']=SECRET_KEY
+    #Register blueprint
+    app.register_blueprint(user)
+    app.register_blueprint(views)
+    return app
