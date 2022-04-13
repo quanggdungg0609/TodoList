55b25bc9cd01add00d9aa5082cdd86a6b3f393d0
quanggdungg0609 <truongquangdung0609@gmail.com>
2022-04-13 14:28:47 +0200

    Todolist app

--- todolist/__init__.py
+++ todolist/__init__.py
@@ -1,28 +1,28 @@
 from logging import NOTSET
 from flask import Flask
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_manager
 from datetime import timedelta
 
 db=SQLAlchemy()
-load_dotenv()
-SECRET_KEY=os.environ.get("SECRET_KEY")
-DB_NAME= os.environ.get("DB_NAME")
+#load_dotenv()
+#SECRET_KEY=os.environ.get("SECRET_KEY")
+#DB_NAME= os.environ.get("DB_NAME")
 
 
 def create_database(app):
-    if not os.path.exists('todolist/'+DB_NAME):
+    if not os.path.exists('todolist/todolist.db'):
         db.create_all(app=app)
         print("DB Created")
         
 def create_app():
     app=Flask(__name__)
-    app.config['SECRET_KEY']=SECRET_KEY
+    app.config['SECRET_KEY']="todolist"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
     #setting database
-    app.config["SQLALCHEMY_DATABASE_URI"]=f"sqlite:///{DB_NAME}"
+    app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///todolist.db"
     db.init_app(app)
     
     from .models import Notes,Users
